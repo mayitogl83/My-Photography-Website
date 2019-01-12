1a5619347c63ca94a0b1df070f13032bb8edc66b
Mario Gonzalez <mayitogl83@gmail.com>
2019-01-11 22:21:14 -0600

    Add content to set the book online page in the app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,44 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import MainPage from './MainPage';
+import BookOnline from './BookOnline';
 import './App.css';
 
 class App extends Component {
 
   state = {
-
+    sessions: [
+      {
+        "id": "Newborn",
+        "title": "Newborn Session",
+        "description": "Describe your service here for a newborn session. What makes it great? Use short catchy text to tell people what you offer, and the benefits they will receive. A great description gets readers in the mood, and makes them more likely to go ahead and book.",
+        "info": "1 hr | Photo Session",
+        "price": "140",
+        "qty": "50",
+        "class": 'newborn-session-img',
+        "mainclass": "exmpl-one"
+      },
+      {
+        "id": "Studio",
+        "title": "Studio Session",
+        "description": "Describe your service here a for studio session. What makes it great? Use short catchy text to tell people what you offer, and the benefits they will receive. A great description gets readers in the mood, and makes them more likely to go ahead and book.",
+        "info": "1/30 hr | Photo Session",
+        "price": "180",
+        "qty": "60",
+        "class": "studio-session-img",
+        "mainclass": "exmpl-two"
+      },
+      {
+        "id": "Couple",
+        "title": "Couple Session",
+        "description": "Describe your service here for a couple session. What makes it great? Use short catchy text to tell people what you offer, and the benefits they will receive. A great description gets readers in the mood, and makes them more likely to go ahead and book.",
+        "info": "2 hrs | Photo Session",
+        "price": "200",
+        "qty": "80",
+        "class": "couple-session-img",
+        "mainclass": "exmpl-three"
+      }
+    ],
     images: [
       {
         "id": "Newborn",
@@ -43,8 +75,14 @@ class App extends Component {
           <Route exact path='/' render={()=>(
             <MainPage
               images={ this.state.images }
+              sessions={ this.state.sessions }
             />
           ) }/>
+          <Route path='/bookonline' render={()=>(
+          <BookOnline
+            onBookingSession={ this.bookingSession }
+            sessions={ this.state.sessions }/>
+        ) }/>
         </div>
       </div>
     );
